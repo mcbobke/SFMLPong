f26041d0a3eb575790e2c2b2f48ad911ba5fd166
Matthew Christopher Bobke <mcbobke@gmail.com>
2014-11-07 23:21:19 -0800

    More formatting

--- SFMLPong/AIFunctions.hpp
+++ SFMLPong/AIFunctions.hpp
@@ -1,7 +1,7 @@
 #ifndef AI_FUNCTIONS_HPP
 #define AI_FUNCTIONS_HPP
 
-#include <SFML/Graphics.hpp>
+#include <SFML\Graphics.hpp>
 
 void aiMove(sf::RectangleShape &pad, sf::RectangleShape &ball, float speed);
 
--- SFMLPong/BallFunctions.cpp
+++ SFMLPong/BallFunctions.cpp
@@ -1,4 +1,4 @@
-#include <SFML/Graphics.hpp>
+#include <SFML\Graphics.hpp>
 #include <string>
 
 void ballReset(sf::RectangleShape &ball, sf::RenderWindow &window)
