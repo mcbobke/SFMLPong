c15fe98bf38bcec0f684cc6918c4b61b66202851
Matthew Bobke <mcbobke@gmail.com>
2015-05-22 20:54:50 -0700

    Added words because Resharper told me to

--- SFMLPong/MainGameState.hpp
+++ SFMLPong/MainGameState.hpp
@@ -13,13 +13,13 @@ class MainGameState : public GameState
 {
 public:
 
-	void Initialize(sf::RenderWindow *window);
+	void Initialize(sf::RenderWindow *window) override;
 
-	void Update(sf::RenderWindow *window);
+	void Update(sf::RenderWindow *window) override;
 
-	void Render(sf::RenderWindow *window);
+	void Render(sf::RenderWindow *window) override;
 
-	void Destroy(sf::RenderWindow *window);
+	void Destroy(sf::RenderWindow *window) override;
 
 private:
 
